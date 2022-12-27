83fad615039bbb4d8933aeb2ec85d85c5bc89a46
interika <interika11@gmail.com>
2022-12-28 01:01:52 +0200

    feat: add validation credit card

--- src/modules/checkout.ts
+++ src/modules/checkout.ts
@@ -1,4 +1,9 @@
 // @ts-nocheck
+import visa from '../img/visa-credit-card.svg';
+import master from '../img/mastercard.svg';
+import americanExpress from '../img/american-express.svg';
+import creditCard from '../img/credit.svg';
+
 class Checkout {
     constructor () {}
 
@@ -7,7 +12,6 @@ class Checkout {
         el.parentNode.classList.add("focus");
     }
      handleBlur = (e) => {
-        // console.log('blur', e.target);
          const el = e.target
         if (el.value === '') {
             el.closest('.input-box').classList.remove("focus")
@@ -20,7 +24,9 @@ class Checkout {
         const phone = form?.querySelector('#checkout-phone')
         const email = form?.querySelector('#checkout-email')
         const address = form?.querySelector('#checkout-address')
-        const rePhone = /^[\d\+][\d\(\)\ -]{8,14}\d$/;
+        const creditCard = form?.querySelector('#card-number')
+        const cvv = form?.querySelector('#card-cvv')
+        const cardExpiry = form?.querySelector('#card-data')        
         const reEmail = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
         
         if (name.value === "" || name.value.split(' ').length < 2 || name.value.split(' ').some(i => i.length < 3)) {
@@ -29,7 +35,7 @@ class Checkout {
             name.closest('.input-box').classList.remove("error")
         }
     
-        if (phone.value === "" || !rePhone.test(phone.value)) {
+        if (phone.value === "" || phone.value.length < 18) {
             phone.closest('.input-box').classList.add("error")
         } else {
             phone.closest('.input-box').classList.remove("error")
@@ -46,6 +52,94 @@ class Checkout {
         } else {
             address.closest('.input-box').classList.remove("error")
         }
+
+        if (creditCard.value === "" || !this.isValidCreditCard(creditCard.value)) {
+            creditCard.closest('.input-box').classList.add("error")
+        } else {
+            creditCard.closest('.input-box').classList.remove("error")
+        }
+        if (!this.isExpireValid(cardExpiry.value)) {
+            cardExpiry.closest('.input-box').classList.add("error")
+        } else {
+            cardExpiry.closest('.input-box').classList.remove("error")
+        }
+        if (cvv.value === "" || cvv?.value.length !== 3 ) {
+            cvv.closest('.input-box').classList.add("error")
+        } else {
+            cvv.closest('.input-box').classList.remove("error")
+        }
+    }
+    isExpireValid = (cardExpire) => {
+        if (cardExpire === '' || cardExpire.length !== 5) {
+            return false;
+        }
+        const year = +cardExpire.split('/')[1]
+        if (year < 22) {
+            return false;
+        }
+        return /(0[1-9]|1[012])\/(\d\d)/.test(cardExpire);
+    }
+    visaCard = (num) => {
+        const cardnum = /^(?:4\d{3}\s\d{4}\s\d{4}\s\d{4})$/;
+        return cardnum.test(num);
+    }
+    masterCard = (num) => {
+        const cardnum = /^(?:5\d{3}\s\d{4}\s\d{4}\s\d{4})$/;
+        return cardnum.test(num);
+    }
+    amexCard = (num) => {
+        const cardnum = /^(?:3\d{3}\s\d{4}\s\d{4}\s\d{4})$/;
+        return cardnum.test(num);
+    }
+    isValidCreditCard = (cardNumber) => {
+        var cardType = null;
+        if (this.visaCard(cardNumber)) {
+            cardType = visa;
+        } else if (this.masterCard(cardNumber)) {
+            cardType = master;
+        } else if (this.amexCard(cardNumber)) {
+            cardType = americanExpress;
+        }
+        return cardType;
+    }
+    handlePhone = (e) => {
+        let x = e.target.value
+          .replace(/\D/g, "")
+          .match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+        e.target.value = !x[2] ? x[1]
+          : `+(${x[1]}) - ${x[2]}` + (!x[3] ? "" : ` - ${x[3]}`);
+    }
+    handleData = (e) => {
+        let x = e.target.value
+          .replace(/\D/g, "")
+          .match(/(0[1-9]|1[012])(\d\d)/);
+        console.log('x', e.target.value.replace(/\D/g, ""), x);
+        if (x) {
+            e.target.value = `${x[1]}\/${x[2]}`;
+        }
+    }
+    handleCardNumber = (e) => {
+        let x = e.target.value
+         .replace(/\D/g, "")
+          .match(/(\d{0,4})(\d{0,4})(\d{0,4})(\d{0,4})/);
+        e.target.value = `${x[1]} ${x[2]} ${x[3]} ${x[4]}`.trim();
+        
+        const image = document.querySelector('.input-image')
+        
+        switch (e.target.value[0]) {
+            case '4':
+                image?.setAttribute('src', visa);
+                break;
+            case '5':
+                image?.setAttribute('src', master);
+                break;
+            case '3':
+                image?.setAttribute('src', americanExpress);
+                break;
+            default: 
+                image?.setAttribute('src', creditCard);
+                break;
+        }
     }
 
     bind = () => {
@@ -58,6 +152,13 @@ class Checkout {
         const form = document.getElementById('form')
         form?.addEventListener('submit', this.handleValidation)
 
+        const phone = form?.querySelector('#checkout-phone')
+        phone?.addEventListener('input', this.handlePhone)
+        const cardData = form?.querySelector('#card-data')
+        cardData?.addEventListener('input', this.handleData)
+        const cardNumber = form?.querySelector('#card-number')
+        cardNumber?.addEventListener('input', this.handleCardNumber)
+
     }
     render = () => {
        
@@ -66,23 +167,46 @@ class Checkout {
             <div class="checkout-container">
                 <h2 class="checkout__title">Personal details</h2>
                 <form class="checkout-form" id="form" autocomplete="off">
-                <div class="input-box">
-                    <label for="checkout-name" class="input-label">First name and Last name</label>
-                    <input class="checkout__input" id="checkout-name" name="checkout.name" type="text" autocomplete="off">
-                </div>
-                <div class="input-box">
-                    <label for="checkout-phone" class="input-label">Phone number</label>
-                    <input class="checkout__input" id="checkout-phone" max='13' min='9' name="checkout.phone" type="tel" autocomplete="off">
-                </div>
-                <div class="input-box">
-                    <label for="checkout-email" class="input-label">E-mail address</label>
-                    <input class="checkout__input" id="checkout-email" name="checkout.email" type="email" autocomplete="off" >
-                </div>
-                <div class="input-box">
-                    <label for="checkout-address" class="input-label">Delivery address</label>
-                    <input class="checkout__input" id="checkout-address" name="checkout.address" type="text" autocomplete="off" >
-                </div>    
-                    <button class="btn btn-checkout">Buy now</button>
+                    <div class='personal-data'>
+                        <div class="input-box">
+                            <label for="checkout-name" class="input-label">First name and Last name</label>
+                            
+                            <input class="checkout__input" id="checkout-name" name="checkout.name" type="text" autocomplete="off">
+                        </div>
+                        <div class="input-box">
+                            <label for="checkout-phone" class="input-label">Phone number</label>
+                            <input class="checkout__input" id="checkout-phone" max='13' min='9' name="checkout.phone" type="tel" autocomplete="off">
+                        </div>
+                        <div class="input-box">
+                            <label for="checkout-email" class="input-label">E-mail address</label>
+                            <input class="checkout__input" id="checkout-email" name="checkout.email" type="email" autocomplete="off" >
+                        </div>
+                        <div class="input-box">
+                            <label for="checkout-address" class="input-label">Delivery address</label>
+                            <input class="checkout__input" id="checkout-address" name="checkout.address" type="text" autocomplete="off" >
+                        </div>    
+                            
+                    </div>
+                     <div class='payment-data'>       
+                        <div class="card-container">
+                            <div class="input-box">
+                                <label for="card-number" class="input-label">Card Number</label>
+                                <img  class="input-image" src="${creditCard}">
+                                <input class="checkout__input" id="card-number"
+                                     name="card.number" type="tel" 
+                                     autocomplete="off">
+                            </div>
+                            <div class="input-box">
+                                <label for="card-data" class="input-label">Expiry (MM/YY)</label>
+                                <input class="checkout__input" id="card-data" name="card.data" type="tel" autocomplete="off">
+                            </div>
+                            <div class="input-box">
+                                <label for="card-number" class="input-label">CVV</label>
+                                <input class="checkout__input" id="card-cvv" name="card.number" type="password" autocomplete="off">
+                            </div>
+                        </div>
+                     </div>
+                     <button class="btn btn-checkout">Buy now</button>
                 </form>
             </div>
         `
